78f1f9a4f8eccb640170c4efb8ac18b2216cdb83
Benedikt Mayer <benedikt_mayer@outlook.de>
2020-12-23 23:43:40 +0100

    added task to 'find max profit in time series'

---
+++ maxprofittimeseries/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Tuple<int, int, double> Solution(double[] pastData){
+    double profit = 0;
+    int bestBuyingIndex = -1;
+    int bestSellingIndex = -1;
+    for(int minIndex = 0; minIndex < pastData.Length; minIndex++){
+        double buyingPrice = pastData[minIndex];
+        for (int maxIndex = minIndex+1; maxIndex < pastData.Length; maxIndex++){
+            double sellingPrice = pastData[maxIndex];
+            if (sellingPrice-buyingPrice > profit){
+                profit = sellingPrice-buyingPrice;
+                bestBuyingIndex = minIndex;
+                bestSellingIndex = maxIndex;
+            }
+        }
+    }
+    return new Tuple<int, int, double>(bestBuyingIndex, bestSellingIndex, profit);
+}
+
+double[] data = {105.0, 101.0, 102.0, 103.0, 100.0};
+Console.WriteLine(Solution(data));
