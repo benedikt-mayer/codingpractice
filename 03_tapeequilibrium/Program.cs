115a839bc6cff35b470d2a5aaa093f7ad4ae15d1
Benedikt Mayer <benedikt_mayer@outlook.de>
2021-01-01 16:31:18 +0100

    added tape equilibrium

---
+++ 03_tapeequilibrium/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// you can also use other imports, for example:
+using System.Linq;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+int solution(int[] A) {
+    // write your code in C# 6.0 with .NET 4.5 (Mono)
+    int total = A.Sum();
+    int leftSide = 0;
+    int minimumDifference = Int32.MaxValue;
+    for(int index = 0; index < A.Length - 1; index++){
+        int tapeNumber = A[index];
+        leftSide += tapeNumber;
+        int rightSide = total - leftSide;
+        int difference = Math.Abs(leftSide - rightSide);
+        minimumDifference = Math.Min(minimumDifference, difference);
+    }
+    return minimumDifference;
+}
+int[] testSet = {-1000, 1000};
+Console.WriteLine("solution correct: " + (solution(testSet) == 2000));
+
